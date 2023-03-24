154161dd39a385e5881a214e1cc862918e036ab2
alex <alex@staticlibs.net>
2023-03-24 21:04:20 +0000

    extern symbols fix

--- src/tools/msvc/build.bat
+++ src/tools/msvc/build.bat
@@ -3,5 +3,5 @@ REM src/tools/msvc/build.bat
 REM all the logic for this now belongs in build.pl. This file really
 REM only exists so you don't have to type "perl build.pl"
 REM Resist any temptation to add any logic here.
-set PATH=%PATH%;C:/projects/postgres/tools/msys/bin;C:/projects/postgres/tools/gnuwin32/bin
+set PATH=%PATH%;C:/projects/postgres/tools/win_flex_bison-2.5.25;C:/projects/postgres/tools/gnuwin32/bin
 @perl %~dp0/build.pl %*
